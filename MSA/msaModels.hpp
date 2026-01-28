6f372ec4b46a94ec70cc588ee19e88b44c28ef67
TubaHorse <sgombas@hotmail.com>
2026-01-28 18:25:17 -0500

    Revert "Made all variants of the MSA available for MSA standalone and IE" Masks cannot be created clientside, separate mod would be needed for this, or this mod would need to be on the server. This reverts commit 8b194fe282263ddcc7ba6b967c713dfb17e882c0.

--- MSA/msaModels.hpp
+++ MSA/msaModels.hpp
@@ -1,19 +1,12 @@
 class JCA_MSA
 {
 	label="APR MSA";
-	options[]={"camo","style","tint"};
+	options[]={"camo","tint"};
 	class camo: JCA_colorBase{};
 	class tint
 	{
 		label="Tint";
 		values[]={"Clear","Tinted"};
-		alwaysSelectable=1;
-		changingame=0;
-	};
-	class style
-	{
-		label="Loadout";
-		values[]={"Empty","Light","Single","Dual","Full"};
 		alwaysSelectable=1;
 		changingame=0;
 	};
