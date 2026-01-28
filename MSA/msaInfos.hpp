8b194fe282263ddcc7ba6b967c713dfb17e882c0
TubaHorse <sgombas@hotmail.com>
2026-01-28 17:26:23 -0500

    Made all variants of the MSA available for MSA standalone and IE

--- MSA/msaInfos.hpp
+++ MSA/msaInfos.hpp
@@ -7,29 +7,179 @@ class JCA_G_AirPurifyingRespirator_04_black_clear_F: JCA_MSA_aceax
 {
 	camo="BLK";
 	tint="Clear";
+	style="Single";
 };
 class JCA_G_AirPurifyingRespirator_04_black_tinted_F: JCA_MSA_aceax
 {
 	camo="BLK";
 	tint="Tinted";
+	style="Single";
 };
 class JCA_G_AirPurifyingRespirator_04_olive_clear_F: JCA_MSA_aceax
 {
 	camo="OLI";
 	tint="Clear";
+	style="Single";
 };
 class JCA_G_AirPurifyingRespirator_04_olive_tinted_F: JCA_MSA_aceax
 {
 	camo="OLI";
 	tint="Tinted";
+	style="Single";
 };
 class JCA_G_AirPurifyingRespirator_04_sand_clear_F: JCA_MSA_aceax
 {
 	camo="SND";
 	tint="Clear";
+	style="Single";
 };
 class JCA_G_AirPurifyingRespirator_04_sand_tinted_F: JCA_MSA_aceax
 {
 	camo="SND";
 	tint="Tinted";
+	style="Single";
+};
+class JCA_G_AirPurifyingRespirator_04_black_clear_empty_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Clear";
+    style="Empty";
+};
+class JCA_G_AirPurifyingRespirator_04_black_tinted_empty_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Tinted";
+    style="Empty";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_clear_empty_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Clear";
+    style="Empty";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_tinted_empty_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Tinted";
+    style="Empty";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_clear_empty_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Clear";
+    style="Empty";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_tinted_empty_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Tinted";
+    style="Empty";
+};
+class JCA_G_AirPurifyingRespirator_04_black_clear_light_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Clear";
+    style="Light";
+};
+class JCA_G_AirPurifyingRespirator_04_black_tinted_light_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Tinted";
+    style="Light";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_clear_light_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Clear";
+    style="Light";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_tinted_light_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Tinted";
+    style="Light";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_clear_light_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Clear";
+    style="Light";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_tinted_light_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Tinted";
+    style="Light";
+};
+class JCA_G_AirPurifyingRespirator_04_black_clear_dual_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Clear";
+    style="Dual";
+};
+class JCA_G_AirPurifyingRespirator_04_black_tinted_dual_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Tinted";
+    style="Dual";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_clear_dual_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Clear";
+    style="Dual";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_tinted_dual_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Tinted";
+    style="Dual";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_clear_dual_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Clear";
+    style="Dual";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_tinted_dual_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Tinted";
+    style="Dual";
+};
+class JCA_G_AirPurifyingRespirator_04_black_clear_full_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Clear";
+    style="Full";
+};
+class JCA_G_AirPurifyingRespirator_04_black_tinted_full_F: JCA_MSA_aceax
+{
+    camo="BLK";
+    tint="Tinted";
+    style="Full";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_clear_full_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Clear";
+    style="Full";
+};
+class JCA_G_AirPurifyingRespirator_04_olive_tinted_full_F: JCA_MSA_aceax
+{
+    camo="OLI";
+    tint="Tinted";
+    style="Full";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_clear_full_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Clear";
+    style="Full";
+};
+class JCA_G_AirPurifyingRespirator_04_sand_tinted_full_F: JCA_MSA_aceax
+{
+    camo="SND";
+    tint="Tinted";
+    style="Full";
 };
--- MSA/msaModels.hpp
+++ MSA/msaModels.hpp
@@ -1,12 +1,19 @@
 class JCA_MSA
 {
 	label="APR MSA";
-	options[]={"camo","tint"};
+	options[]={"camo","style","tint"};
 	class camo: JCA_colorBase{};
 	class tint
 	{
 		label="Tint";
 		values[]={"Clear","Tinted"};
+		alwaysSelectable=1;
+		changingame=0;
+	};
+	class style
+	{
+		label="Loadout";
+		values[]={"Empty","Light","Single","Dual","Full"};
 		alwaysSelectable=1;
 		changingame=0;
 	};
