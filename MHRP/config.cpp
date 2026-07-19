f5c48c96224b18b4252c35165910b565db2b40d9
TubaHorse <sgombas@hotmail.com>
2026-07-19 02:17:47 -0400

    Completely removed ace hearing values

--- MHRP/config.cpp
+++ MHRP/config.cpp
@@ -12,29 +12,6 @@ class CfgPatches
     };
 };
 
-/* class CfgWeapons
-{
-    class JCA_H_HelmetHBK_base_F;
-    class JCA_H_HelmetHBK_chops_base_F: JCA_H_HelmetHBK_base_F
-    {
-        ace_hearing_protection=0.75;
-        ace_hearing_lowerVolume=0;
-        ace_hearing_hasEHP=1;
-    };
-    class JCA_H_HelmetHBK_ear_base_F: JCA_H_HelmetHBK_base_F
-    {
-        ace_hearing_protection=0.75;
-        ace_hearing_lowerVolume=0;
-        ace_hearing_hasEHP=1;
-    };
-    class JCA_H_HelmetHBK_headset_base_F: JCA_H_HelmetHBK_base_F
-    {
-        ace_hearing_protection=0.75;
-        ace_hearing_lowerVolume=0;
-        ace_hearing_hasEHP=1;
-    };
-}; */
-
 class XtdGearModels
 {
     class JCA_colorBase;
